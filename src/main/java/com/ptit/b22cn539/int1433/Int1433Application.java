54a6dd9fdbe1973f1d9f1a7b98c5d445decb0379
Nguyen Tien Minh <nguyentienminhntm07102004@gmail.com>
2025-10-25 21:20:36 +0700

    Init project

---
+++ src/main/java/com/ptit/b22cn539/int1433/Int1433Application.java
@@ -0,0 +1,13 @@
+package com.ptit.b22cn539.int1433;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Int1433Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Int1433Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ptit/b22cn539/int1433/Int1433ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptit.b22cn539.int1433;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Int1433ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
