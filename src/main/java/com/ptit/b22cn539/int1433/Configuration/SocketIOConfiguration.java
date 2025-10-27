cca01618cc3284ca8984e43ac4af546097dbd5cb
Nguyen Tien Minh <nguyentienminhntm07102004@gmail.com>
2025-10-27 23:53:33 +0700

    Change version netty-socketio from 1.7.17 to 2.0.13

--- src/main/java/com/ptit/b22cn539/int1433/Configuration/SocketIOConfiguration.java
+++ src/main/java/com/ptit/b22cn539/int1433/Configuration/SocketIOConfiguration.java
@@ -26,6 +26,7 @@ public class SocketIOConfiguration {
         com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
         config.setHostname(host);
         config.setPort(port);
+        config.setEnableCors(false);
         SocketIOServer server = new SocketIOServer(config);
         server.addListeners(this);
         server.start();
